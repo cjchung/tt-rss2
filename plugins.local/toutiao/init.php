0b3cc3ee7679d747de0adc68a488f6f8bde5ed07
cjchung <cjchung@users.noreply.github.com>
2022-01-25 02:09:14 +0800

    toutiao refactoring

--- plugins.local/toutiao/init.php
+++ plugins.local/toutiao/init.php
@@ -16,10 +16,7 @@ class toutiao extends Plugin{
 
 	function init($host) {
 		$host->add_hook($host::HOOK_FEED_FETCHED, $this);
-//		$host->add_hook($host::HOOK_FEED_BASIC_INFO, $this);
-//		$host->add_hook($host::HOOK_SUBSCRIBE_FEED, $this);
 		$host->add_hook($host::HOOK_ARTICLE_FILTER, $this);
-//		$host->add_hook($host::HOOK_IFRAME_WHITELISTED, $this);
 	}
 
 	/**
@@ -44,7 +41,6 @@ class toutiao extends Plugin{
 			return $feed_data;
 		}
 
-		$force_refetch = isset($_REQUEST["force_refetch"]);
 		$force_rehash = isset($_REQUEST["force_rehash"]);
 
 		$doc = new DOMDocument();
@@ -62,15 +58,24 @@ class toutiao extends Plugin{
 		$rss = '<?xml version="1.0" encoding="UTF-8" ?><rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:slash="http://purl.org/rss/1.0/modules/slash/"><channel><title>' . htmlspecialchars($rssTitleText) . '</title><link>' . htmlspecialchars($fetch_url) . '</link>';
 
 		$itemNodes = $xpath->query('//div[@class="post"]');
-
+		$counter=0;
 		foreach ($itemNodes as $itemNode) {
 			$linkNode=$xpath->query('.//h3/a', $itemNode)[0];
 			$link=$linkNode->getAttribute('href');
 			if(substr($link,0,4)!=='http'){
 				$link = 'https://toutiao.io'.$link;
 			}
+			if(strpos($link,'https://toutiao.io/shares')===0){
+				//subscriptions
+				$link=$linkNode->parentNode->parentNode->getAttribute('data-url');
+			}
+			if(strpos($link,'https://toutiao.io/posts/')===0){
+				//subscriptions
+				$link='https://toutiao.io/k/'.substr($link,25);
+			}
 			$entry_guid = $link;
 			$entry_guid_hashed = json_encode(["ver" => 2, "uid" => $owner_uid, "hash" => 'SHA1:' . sha1($entry_guid)]);
+			Debug::log("link=$link",Debug::LOG_VERBOSE);
 
 			$title=$linkNode->nodeValue;
 			$author=$xpath->query('.//div[@class="subject-name"]/a', $itemNode)[0]->nodeValue;
@@ -86,9 +91,11 @@ class toutiao extends Plugin{
 					continue;
 				}
 			}
-			$article=['link' => $link, 'author' => $author, 'title' => $title];
+			$meta=trim($xpath->evaluate('string(.//div[@class="meta"]/text())', $itemNode));
+			$article=['link' => $link, 'author' => $author, 'title' => $title, 'meta'=>$meta];
 			Debug::log("link: $link",Debug::LOG_VERBOSE);
 			Debug::log("title: $title",Debug::LOG_VERBOSE);
+			Debug::log("meta: $meta",Debug::LOG_VERBOSE);
 //			$likes=(int)$xpath->evaluate('string(.//a[contains(@href,"/likes/post/")]/span)', $itemNode);
 //			$likes=$xpath->evaluate('./div[0]/div[0]/a[0]/span[0]', $itemNode)[0]->nodeValue;
 //			Debug::log("likes: $likes",Debug::LOG_VERBOSE);
@@ -109,12 +116,14 @@ class toutiao extends Plugin{
 			Debug::log("replies: $replies",Debug::LOG_VERBOSE);
 			$likes += $replies * 20;
 			Debug::log("comments(score): $likes",Debug::LOG_VERBOSE);
-			$this->parse_page($article);
+			if(!$this->parse_page($article)){
+				continue;
+			}
 
 			$rss .= "<item><title>" . htmlspecialchars($article['title']) .
 				"</title><link>" . htmlspecialchars($article['link']) .
 				"</link><guid>".htmlspecialchars($entry_guid)."</guid><author>" . htmlspecialchars($article['author']) .
-				"</author><pubDate>".$article['pubDate']."</pubDate><description>".htmlspecialchars($article['content'])."</description>";
+				"</author><description>".htmlspecialchars($article['content'])."</description>";
 
 			if($likes)$rss .= "<slash:comments>$likes</slash:comments>";
 			if(key_exists('tags', $article)){
@@ -122,8 +131,12 @@ class toutiao extends Plugin{
 					$rss .= "<category>$tag</category>";
 				}
 			}
+			if(key_exists('pubDate', $article)){
+				$rss .= "<pubDate>".$article['pubDate']."</pubDate>";
+			}
 
 			$rss .= "</item>";
+			if($counter++>=10) break;
 
 		}
 		$rss .= "</channel></rss>";
@@ -136,10 +149,6 @@ class toutiao extends Plugin{
 			return $article;
 		}
 
-		if (empty($article['content'])) {
-//			$this->parse_page($article);
-		}
-
 		if($article["num_comments"]){
 			$article["score_modifier"]=$article["num_comments"];
 		}
@@ -147,39 +156,25 @@ class toutiao extends Plugin{
 	}
 
 
-	protected function  fetch_and_cache(&$article) {
-		$link = $article['link'];
-		$cache_filename = Config::get(Config::CACHE_DIR) . "/feeds/toutiao-" . sha1($link) . ".xml";
-		if(file_exists($cache_filename)){
-			$obj=unserialize(file_get_contents($cache_filename));
-			$article['link'] = $obj['effective_url'];
-			if(function_exists('bzdecompress')){
-				return bzdecompress($obj['html']);
-			}else{
-				return gzdeflate($obj['html']);
-			}
-		}
-		$html=UrlHelper::fetch(["url" => $link]);
-		if(!$html){
-			Debug::log("fetch failed: $link");
-		}
-		$link = UrlHelper::$fetch_effective_url;
-		$article['link'] = $link;
-		if(function_exists('bzdecompress')){
-			file_put_contents($cache_filename,serialize(['effective_url'=> $link, 'html'=> bzcompress($html)]));
-		}else{
-			file_put_contents($cache_filename,serialize(['effective_url'=> $link, 'html'=> gzcompress($html)]));
-		}
-		return $html;
-
-	}
 	protected function  parse_page(&$article){
-		$html = $this->fetch_and_cache($article);
-		if(!$html) return;
+		$html=UrlHelper::fetch(["url" => $article['link']]);
+		if(!$html) return false;
+		$fetch_effective_url = UrlHelper::$fetch_effective_url;
 		$doc = new DOMDocument();
 		$doc->loadHTML('<?xml encoding="utf-8" ?>'.$html);
 		$xpath = new DOMXPath($doc);
-		if(strpos($article['link'],'weixin.qq')){
+		if(strpos($article['meta'],'weixin') || strpos($fetch_effective_url,'weixin.qq')){
+			if(!strpos($fetch_effective_url,'weixin.qq')){
+				Debug::log("not redirected to weixin");
+				$link=$xpath->evaluate('string(//a[small[text()="(查看原文)"]]/@href)');
+				if($link){
+					Debug::log("redirect link found: $link",Debug::LOG_EXTENDED);
+					$html=UrlHelper::fetch(["url" => $link]);
+				}else{
+					Debug::log("redirect link for weixin not exist.");
+					return false;
+				}
+			}
 			$contentNode = $xpath->query('//div[contains(@class,"rich_media_content")]')[0];
 			foreach ($xpath->query('.//img', $contentNode) as $img){
 				$img->setAttribute('src', $img->getAttribute('data-src'));
@@ -193,14 +188,16 @@ class toutiao extends Plugin{
 				$article['tags'][]=$t;
 			}
 			$article['content']=$doc->saveHTML($contentNode);
-		}else if(preg_match('/https:\/\/toutiao\.io\/posts\/.+?\?/', $article['link'],$m)){
-			$link=$xpath->evaluate('string(//a[small[text()="(查看原文)"]]/@href)');
-			$article['link'] = 'https://toutiao.io'.$link;
-			$this->parse_page($article);
+		}elseif(strpos($article['meta'],'toutiao')!==false){
+			$contentNode = $xpath->query('//div[contains(@class,"post-body")]')[0];
+			$article['content']=$doc->saveHTML($contentNode);
+			foreach ($xpath->query('//div[@class="post-tags"]/a') as $tag){
+				$article['tags'][]=$tag->nodeValue;
+			}
 		}else{
 			$article['content']=$html;
-			Debug::log("unknown source, full html attached",Debug::LOG_VERBOSE);
 		}
+		return true;
 	}
 
 }
